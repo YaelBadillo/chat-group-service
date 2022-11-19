34dabc39e6fb07b16608d0e960cfe7150e5888b7
YaelBadillo <yael.badillos@gmail.com>
2022-11-18 21:53:21 -0600

    Feat: add dto validations

--- src/user/dto/delete-user.dto.ts
+++ src/user/dto/delete-user.dto.ts
@@ -1,3 +1,8 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class DeleteUserDto {
+  @IsString()
+  @Length(6, 40)
+  @IsNotEmpty()
   password: string;
 }
--- src/user/dto/update-password.dto.ts
+++ src/user/dto/update-password.dto.ts
@@ -1,5 +1,23 @@
+import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
+import { Match } from '../../common/decorators';
+
 export class UpdatePasswordDto {
+  @IsString()
+  @Length(6, 40)
+  @IsNotEmpty()
   oldPassword: string;
+
+  @IsString()
+  @Length(6, 40)
+  @IsNotEmpty()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   newPassword: string;
+
+  @IsString()
+  @Length(6, 40)
+  @IsNotEmpty()
+  @Match('password', { message: 'password do not match' })
   newConfirmPassword: string;
 }
