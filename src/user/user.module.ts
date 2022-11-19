9fb683ce3e17d04009f3d30e8607c5b8aaa488f0
YaelBadillo <yael.badillos@gmail.com>
2022-11-18 21:37:05 -0600

    Fix: import password module tu the user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { UserController } from './controllers';
 import { UserService } from './services';
+import { PasswordModule } from '../shared';
 
 @Module({
+  imports: [PasswordModule],
   controllers: [UserController],
   providers: [UserService]
 })
