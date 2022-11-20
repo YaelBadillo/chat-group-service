44d2170420976ce58f48aeb2883d5a269ec1042c
YaelBadillo <yael.badillos@gmail.com>
2022-11-20 01:30:35 -0600

    Feat: add channel entity to the type orm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards';
 import { UserModule } from './user/user.module';
 import { configuration } from './config';
-import { User } from './entities';
+import { User, Channel } from './entities';
 import { ChannelModule } from './channel/channel.module';
 
 @Module({
@@ -24,7 +24,7 @@ import { ChannelModule } from './channel/channel.module';
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         ...configService.get<TypeOrmModuleOptions>('database'),
-        entities: [User],
+        entities: [User, Channel],
         namingStrategy: new SnakeNamingStrategy(),
         synchronize: false,
       }),
