c9e226ab85b784bd32c0d7d630c996c359ac48d7
YaelBadillo <yael.badillos@gmail.com>
2022-11-23 14:36:02 -0600

    Feat: apply class validator to the update channel dto

--- src/channel/dto/update-channel.dto.ts
+++ src/channel/dto/update-channel.dto.ts
@@ -1,7 +1,19 @@
+import { IsEnum, IsNotEmpty, IsString, Length } from 'class-validator';
+
 import { SpaceType } from '../../common/enums';
 
 export class UpdateChannelDto {
+  @IsString()
+  @Length(3, 30)
+  @IsNotEmpty()
   name: string;
+
+  @IsEnum(SpaceType)
+  @IsNotEmpty()
   space: SpaceType;
+
+  @IsString()
+  @Length(0, 255)
+  @IsNotEmpty()
   description: string;
 }
