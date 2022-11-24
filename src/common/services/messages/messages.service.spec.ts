b1c39ff01c38e29ea7a91ab4ca750d2d6db02c90
YaelBadillo <yael.badillos@gmail.com>
2022-11-24 14:28:31 -0600

    Feat: add save method to the messages service

--- src/common/services/messages/messages.service.spec.ts
+++ src/common/services/messages/messages.service.spec.ts
@@ -1,18 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+import { Chance } from 'chance';
+import { mock } from 'jest-mock-extended';
+
 import { MessagesService } from './messages.service';
+import { Message } from '../../../entities';
+import { messageMockFactory } from '../../../../test/utils/entity-mocks';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('MessagesService', () => {
   let service: MessagesService;
+  let messageRepositoryMock: jest.Mocked<Repository<Message>>;
+
+  let chance: Chance.Chance;
 
   beforeEach(async () => {
+    messageRepositoryMock = mock<Repository<Message>>();
+
+    chance = new Chance();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MessagesService],
+      providers: [
+        MessagesService,
+        {
+          provide: getRepositoryToken(Message),
+          useValue: messageRepositoryMock,
+        },
+      ],
     }).compile();
 
     service = module.get<MessagesService>(MessagesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('save method', () => {
+    let messageMock: Message;
+
+    beforeEach(() => {
+      messageMock = messageMockFactory(chance);
+    });
+
+    it('should create/updated message', async () => {
+      const expectedMessage: Message = { ...messageMock };
+
+      await service.save(messageMock);
+
+      expect(messageRepositoryMock.save).toBeCalledTimes(1);
+      expect(messageRepositoryMock.save).toBeCalledWith(expectedMessage);
+    });
+
+    it('should return the created/updated message', async () => {
+      const expectedMessage: Message = { ...messageMock };
+      messageRepositoryMock.save.mockReturnValue((async () => messageMock)());
+
+      const result: Message = await service.save(messageMock);
+
+      expect(result).toEqual(expectedMessage);
+    });
+
+    it('should throw if the message could not be created or updated', async () => {
+      const expectedMessageError = 'Message could not be created or updated';
+      messageRepositoryMock.save.mockImplementation(async () => {
+        throw new Error();
+      });
+
+      const execute = () => service.save(messageMock);
+
+      await expect(execute).rejects.toThrowError(InternalServerErrorException);
+      await expect(execute).rejects.toThrow(expectedMessageError);
+    });
   });
 });
--- src/common/services/messages/messages.service.ts
+++ src/common/services/messages/messages.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { Message } from '../../../entities';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectRepository(Message)
+    private readonly messageRepository: Repository<Message>,
+  ) {}
+
+  public async save(message: Message): Promise<Message> {
+    try {
+      const newMessage: Message = await this.messageRepository.save(message);
+      return newMessage;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Message could not be created or updated',
+      );
+    }
+  }
+}
