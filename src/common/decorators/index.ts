1c8fd86baa6f1e9218c30fa94efd3e11dff6b378
YaelBadillo <yael.badillos@gmail.com>
2022-11-25 02:01:09 -0600

    Feat: add ws jwt auth guard and decorator to its respective index

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -3,3 +3,4 @@ export * from './user-from-request.decorator';
 export * from './match.decorator';
 export * from './channel-owner.decorator';
 export * from './channel-from-request.decorator';
+export * from './ws-jwt-auth.decorator';
