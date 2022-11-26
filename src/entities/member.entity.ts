a3b127f3abe9ee102e74538c99ee5c958510c266
YaelBadillo <yael.badillos@gmail.com>
2022-11-26 16:29:28 -0600

    Feat: add member entity

---
+++ src/entities/member.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+import { DatesHelper } from './utils';
+import { InvitationStatus, MemberRole, RequestStatus } from '../common/enums';
+
+@Entity('members')
+export class Member extends DatesHelper {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  userId: string;
+
+  @Column({ type: 'enum', enum: MemberRole, default: MemberRole.MEMBER })
+  role: MemberRole;  
+
+  @Column({ type: 'varchar' })
+  channelId: string;
+
+  @Column({ type: 'enum', enum: InvitationStatus })
+  invitationStatus: InvitationStatus;
+
+  @Column({ type: 'enum', enum: RequestStatus })
+  requestStatus: RequestStatus;
+
+  @Column({ type: 'boolean' })
+  deleted: boolean;
+
+  @Column({ type: 'timestamptz' })
+  expireAt: Date;
+
+  @Column({ type: 'varchar' })
+  createdBy: string;
+
+  @Column({ type: 'varchar' })
+  updatedBy: string;
+}
