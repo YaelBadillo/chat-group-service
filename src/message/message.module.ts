96a5835de56f30e1579052aa5009d389b50fae7d
YaelBadillo <yael.badillos@gmail.com>
2022-11-25 22:50:56 -0600

    Remove message module

--- src/message/message.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { MessageGateway } from './gateways';
-import { MessageService } from './services';
-@Module({
-  providers: [MessageGateway, MessageService]
-})
-export class MessageModule {}
