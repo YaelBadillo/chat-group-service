c46c8b9c3882454ad4736be6ce96aa754c0d0415
YaelBadillo <yael.badillos@gmail.com>
2022-11-28 14:25:06 -0600

    Feat: add handle message dto

--- src/channel/dto/index.ts
+++ src/channel/dto/index.ts
@@ -1,3 +1,4 @@
 export * from './create-channel.dto';
 export * from './update-channel.dto';
 export * from './delete-channel.dto';
+export * from './handle-message.dto';
--- src/channel/gateways/channel.gateway.ts
+++ src/channel/gateways/channel.gateway.ts
@@ -13,12 +13,15 @@ import { Socket } from 'socket.io';
 import { MembersService } from '../../common/services';
 import { Member } from '../../entities';
 import { WsJwtAuth } from '../../common/decorators';
+import { HandleMessageDto } from '../dto';
+import { ChannelService } from '../services';
 
 @WebSocketGateway({ namespace: 'channel' })
 export class ChannelGateway
-  implements OnGatewayConnection, OnGatewayDisconnect
+  implements OnGatewayConnection
 {
-  constructor(private readonly membersService: MembersService) {}
+  constructor(
+    private readonly membersService: MembersService) {}
 
   public async handleConnection(client: Socket): Promise<void> {
     const userId: string | string[] = client.handshake.query?.userId;
@@ -36,16 +39,17 @@ export class ChannelGateway
     userMembers.forEach((userMember) => client.join(userMember.channelId));
   }
 
-  public handleDisconnect(client: Socket) {}
-
   @SubscribeMessage('message')
   @WsJwtAuth()
-  public handleMessage(
-    @MessageBody() data: { channelId: string, content: string },
+  public async handleMessage(
+    @MessageBody() handleMessageDto: HandleMessageDto,
     @ConnectedSocket() client: Socket,
   ) {
-    const message = { content: data.content, userName: client.data.user.name };
+    const message = { 
+      content: handleMessageDto.content, 
+      userName: client.data.user.name 
+    };
 
-    client.to(data.channelId).emit(JSON.stringify(message));
+    client.to(handleMessageDto.channelId).emit(JSON.stringify(message));
   }
 }
