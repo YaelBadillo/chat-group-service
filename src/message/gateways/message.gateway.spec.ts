205f9d2ab6a2cc6857dadbd1f62c0bdad4a07a26
YaelBadillo <yael.badillos@gmail.com>
2022-11-28 20:02:40 -0600

    Feat: add handle connection to the message gateway

--- src/message/gateways/message.gateway.spec.ts
+++ src/message/gateways/message.gateway.spec.ts
@@ -1,20 +1,111 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BadRequestException } from '@nestjs/common';
+
+import { Socket } from 'socket.io';
+import { mock } from 'jest-mock-extended';
+import { Chance } from 'chance';
 
 import { MessageGateway } from './message.gateway';
 import { MessageService } from '../services';
+import { MembersService } from '../../common/services';
+import { Member } from '../../entities';
+import { memberMockFactory } from '../../../test/utils/entity-mocks';
 
 describe('MessageGateway', () => {
   let gateway: MessageGateway;
+  let messageServiceMock: jest.Mocked<MessageService>;
+  let membersServiceMock: jest.Mocked<MembersService>;
+
+  let chance: Chance.Chance;
 
   beforeEach(async () => {
+    messageServiceMock = mock<MessageService>();
+    membersServiceMock = mock<MembersService>();
+
+    chance = new Chance();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MessageGateway, MessageService],
+      providers: [
+        MessageGateway, 
+        MessageService,
+        {
+          provide: MessageService,
+          useValue: messageServiceMock,
+        },
+        {
+          provide: MembersService,
+          useValue: membersServiceMock,
+        },
+      ],
     }).compile();
 
     gateway = module.get<MessageGateway>(MessageGateway);
   });
 
-  it('should be defined', () => {
-    expect(gateway).toBeDefined();
+  describe('handleConnection method', () => {
+    let socketMock: Socket;
+    let userIdMock: string;
+
+    beforeEach(() => {
+      socketMock = mock<Socket>();
+      userIdMock = chance.string({ length: 20 });
+      socketMock.handshake.query = {
+        userId: userIdMock,
+      };
+
+      membersServiceMock.getAllByUserId.mockReturnValue(
+        (async () => [new Member()])(),
+      );
+    });
+
+    it('should throw if no userId is provided', async () => {
+      const expectedErrorMessage = 'Please provide a user id';
+      socketMock.handshake.query = {};
+
+      const execute = () => gateway.handleConnection(socketMock);
+
+      await expect(execute).rejects.toThrowError(BadRequestException);
+      await expect(execute).rejects.toThrow(expectedErrorMessage);
+    });
+
+    it('should throw if the user is an array of strings', async () => {
+      const expectedErrorMessage =
+        'The userId query parameter should be a string';
+      socketMock.handshake.query = {
+        userId: ['', ''],
+      };
+
+      const execute = () => gateway.handleConnection(socketMock);
+
+      await expect(execute).rejects.toThrowError(BadRequestException);
+      await expect(execute).rejects.toThrow(expectedErrorMessage);
+    });
+
+    it('should get members of the client', async () => {
+      await gateway.handleConnection(socketMock);
+
+      expect(membersServiceMock.getAllByUserId).toBeCalledTimes(1);
+      expect(membersServiceMock.getAllByUserId).toBeCalledWith(userIdMock);
+    });
+
+    it('should join the client to all his channels rooms', async () => {
+      const userMembersLength = 3;
+      const userMembersMock: Member[] = new Array(userMembersLength)
+        .fill(null)
+        .map(() => memberMockFactory(chance));
+      const expectedUserMembers: Member[] = userMembersMock.map(
+        (member) => member,
+      );
+      membersServiceMock.getAllByUserId.mockReturnValue(
+        (async () => userMembersMock)(),
+      );
+
+      await gateway.handleConnection(socketMock);
+
+      expect(socketMock.join).toBeCalledTimes(userMembersLength);
+      expectedUserMembers.forEach((userMember) =>
+        expect(socketMock.join).toBeCalledWith(userMember.channelId),
+      );
+    });
   });
 });
--- src/message/gateways/message.gateway.ts
+++ src/message/gateways/message.gateway.ts
@@ -1,12 +1,36 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, OnGatewayConnection } from '@nestjs/websockets';
+import { BadRequestException } from '@nestjs/common';
+
+import { Socket } from 'socket.io';
 
 import { MessageService } from '../services';
 import { CreateMessageDto } from '../dto/create-message.dto';
 import { UpdateMessageDto } from '../dto/update-message.dto';
+import { MembersService } from '../../common/services';
+import { Member } from '../../entities';
 
 @WebSocketGateway()
-export class MessageGateway {
-  constructor(private readonly messageService: MessageService) {}
+export class MessageGateway implements OnGatewayConnection {
+  constructor(
+    private readonly messageService: MessageService,
+    private readonly membersService: MembersService,
+  ) {}
+
+  public async handleConnection(client: Socket): Promise<void> {
+    const userId: string | string[] = client.handshake.query?.userId;
+    if (!userId) throw new BadRequestException('Please provide a user id');
+
+    if (userId instanceof Array<string>)
+      throw new BadRequestException(
+        'The userId query parameter should be a string',
+      );
+
+    const userMembers: Member[] = await this.membersService.getAllByUserId(
+      userId,
+    );
+
+    userMembers.forEach((userMember) => client.join(userMember.channelId));
+  }
 
   @SubscribeMessage('createMessage')
   create(@MessageBody() createMessageDto: CreateMessageDto) {
