e3273f96fb9963b9a1b28a0dbc69a3f32f777843
YaelBadillo <yael.badillos@gmail.com>
2023-01-26 18:41:02 -0600

    Refact: refact channel gateway

--- src/channel/gateways/channel.gateway.ts
+++ src/channel/gateways/channel.gateway.ts
@@ -1,79 +1,35 @@
-import {
-  BadRequestException,
-  UnauthorizedException,
-  Logger,
-} from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import {
   WebSocketGateway,
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { JwtService, JwtModuleOptions } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 
 import { MembersService, UsersService } from '../../common/services';
-import { Channel, Member, User } from '../../entities';
+import { Channel } from '../../entities';
+import { VerifyChannelConnectionGateway } from '../../common/gateways';
 
 @WebSocketGateway({ namespace: 'channel' })
 export class ChannelGateway
+  extends VerifyChannelConnectionGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
-  private readonly logger = new Logger(ChannelGateway.name);
-
   @WebSocketServer()
   private readonly server: Server;
 
   constructor(
-    private readonly jwtService: JwtService,
-    private readonly configService: ConfigService,
-    private readonly usersService: UsersService,
-    private readonly membersService: MembersService,
-  ) {}
-
-  public async handleConnection(client: Socket): Promise<void> {
-    const token: string =
-      client.handshake.auth?.token || client.handshake.headers?.token;
-    if (!token) throw new UnauthorizedException('No token provided');
-
-    let name: string;
-    try {
-      name = this.jwtService.verify<{ name: string }>(token, {
-        ...this.configService.get<JwtModuleOptions>('jwt'),
-      }).name;
-    } catch (error) {
-      this.logger.error('Invalid token');
-      client.emit('validation', 'Invalid token');
-      client.disconnect();
-      return;
-    }
-
-    const user: User = await this.usersService.findOneByName(name);
-    if (!user)
-      throw new BadRequestException(
-        'User does not exists, please authenticate',
-      );
-
-    client.data.user = user;
-
-    const userMembers: Member[] = await this.membersService.getAllByUserId(
-      user.id,
-    );
-
-    userMembers.forEach((userMember) => client.join(userMember.channelId));
-  }
-
-  public async handleDisconnect(client: Socket): Promise<void> {
-    const user: User = client.data.user;
-
-    delete client.data.user;
-
-    const userMembers: Member[] = await this.membersService.getAllByUserId(
-      user.id,
-    );
-    userMembers.forEach((userMember) => client.leave(userMember.channelId));
+    protected readonly jwtService: JwtService,
+    protected readonly configService: ConfigService,
+    protected readonly usersService: UsersService,
+    protected readonly membersService: MembersService,
+  ) {
+    super(jwtService, configService, usersService, membersService);
+    this.logger = new Logger(ChannelGateway.name);
   }
 
   public notifyUpdateToEachActiveMembers(updatedChannel: Channel): void {
