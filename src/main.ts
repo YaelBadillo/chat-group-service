6ad6f9f18eae4f5327a82f6ae6ceede6394c7b81
YaelBadillo <yael.badillos@gmail.com>
2023-02-25 22:44:45 -0600

    Feat: add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
@@ -7,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(cookieParser());
 
   await app.listen(3000);
 }
